b5d4bae30a1d427417ff87497e9c7189688395a5
coachphil5 <coachphil5@gmail.com>
2014-03-19 22:03:46 -0700

    First Commit.  Trying to figure out system.

---
+++ src/InsertFileDataToJTable.java
@@ -0,0 +1,52 @@
+import javax.swing.table.AbstractTableModel;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by JOINTJUICE on 3/19/14.
+ */
+public class InsertFileDataToJTable extends AbstractTableModel {
+    Vector data;
+    Vector columns;
+
+    public InsertFileDataToJTable() {
+        String line;
+        data = new Vector();
+        columns = new Vector();
+        try {
+            FileInputStream fis = new FileInputStream(System.getProperty("user.home") +"/Desktop/phonelist.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            StringTokenizer st1 = new StringTokenizer(br.readLine(), "\t");
+            //System.out.println(st1.nextToken());
+            while (st1.hasMoreTokens())
+                columns.addElement(st1.nextToken());
+            while ((line = br.readLine()) != null) {
+                StringTokenizer st2 = new StringTokenizer(line, "\t");
+                while (st2.hasMoreTokens())
+                    data.addElement(st2.nextToken());
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getRowCount() {
+        return data.size() / getColumnCount();
+    }
+
+    public int getColumnCount() {
+        return columns.size();
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return (String) data.elementAt((rowIndex * getColumnCount())
+                + columnIndex);
+    }
+    public String getColumnName(int col) {
+        return columns.get(col).toString();
+    }
+}
---
+++ src/jTalbeListTxt.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.Component;
+import java.awt.Color;
+
+/**
+ * Created by JOINTJUICE on 3/19/14.
+ */
+public class jTalbeListTxt {
+  private JPanel panel1;
+  /*  private JTable table1;
+    private JScrollPane jScroll1;
+*/
+    public static void main(String[] args) {
+        InsertFileDataToJTable model = new InsertFileDataToJTable();
+        JTable table = new JTable(model) {
+            public Component prepareRenderer(TableCellRenderer renderer, int Index_row, int Index_col) {
+                // get the current row
+                Component comp = super.prepareRenderer(renderer, Index_row, Index_col);
+                // even index, not selected
+                if (Index_row % 2 == 0 && !isCellSelected(Index_row, Index_col)) {
+                    comp.setBackground(Color.lightGray);
+                } else {
+                    comp.setBackground(Color.white);
+                }
+                return comp;
+            }
+        };
+
+        JScrollPane scrollpane = new JScrollPane(table);
+        JPanel panel = new JPanel();
+        panel.add(scrollpane);
+        JFrame frame = new JFrame();
+        frame.add(panel, "Center");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+}
+
+
+
+
+
+/*
+
+      InsertFileDataToJTable model = new InsertFileDataToJTable();
+        JTable table = new JTable();
+        table.setModel(model);
+        JScrollPane scrollpane = new JScrollPane(table);
+        JPanel panel = new JPanel();
+        panel.add(scrollpane);
+        JFrame frame = new JFrame();
+        frame.add(panel, "Center");
+        frame.pack();
+        frame.setVisible(true);
+*/
+
+
+
+/*
+InsertFileDataToJTable model = new InsertFileDataToJTable();
+JFrame frame = new JFrame("Table Practice");
+table1 = new Tabel1 InsertFileDataToJTable();
+        // table1.setModel(new model);
+        frame.setContentPane(new jTalbeListTxt().panel1);
+        //panel1.add(table1.setModel(model));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);*/
+
+
+/*
+class MyRenderer implements TableCellRenderer {
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                                                   boolean hasFocus, int row, int column) {
+        JTextField editor = new JTextField();
+        if (value != null)
+            editor.setText(value.toString());
+        editor.setBackground((row % 2 == 0) ? Color.white : Color.cyan);
+        return editor;
+    }
+}
+*/
